ee3d5eef3ed6a0ed604bba60f530673219395a7d
mariokirby1703 <tomgoettert@gmail.com>
2026-08-16 02:38:48 +0200

    add ap icon made out of marbles for the client in the ap launcher

--- worlds/marbles_balance_challenge/components.py
+++ worlds/marbles_balance_challenge/components.py
@@ -1,6 +1,19 @@
-from worlds.LauncherComponents import Component, SuffixIdentifier, Type, components, launch
+from worlds.LauncherComponents import (
+    Component,
+    SuffixIdentifier,
+    Type,
+    components,
+    icon_paths,
+    launch,
+)
 
 from .world_constants import GAME_NAME
+
+
+MARBLES_ICON = "marbles_balance_challenge"
+icon_paths[MARBLES_ICON] = (
+    "ap:worlds.marbles_balance_challenge/assets/Marble Logo.png"
+)
 
 
 def run_client(*args: str) -> None:
@@ -15,6 +28,7 @@ components.append(
         func=run_client,
         game_name=GAME_NAME,
         component_type=Type.CLIENT,
+        icon=MARBLES_ICON,
         file_identifier=SuffixIdentifier(".apmbc"),
         supports_uri=True,
     )
